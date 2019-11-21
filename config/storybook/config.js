6b220db9f1196475015cb95ca8aa1dcba852b2f8
NeeleshJoshi <neeleshjoshi@pop-os.localdomain>
2019-11-21 20:49:15 +0000

    Updated the config ready for first component

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,10 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { configure } from '@storybook/vue'
 
-const req = require.context('../../src/stories', true, /.stories.js$/)
+const req = require.context('../../src/', true, /.stories.(t|j)s$/)
 
 function loadStories() {
   req.keys().forEach(filename => req(filename))
 }
 
 configure(loadStories, module)
+
+
+
