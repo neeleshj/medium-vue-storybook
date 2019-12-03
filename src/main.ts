d9756b5a23e245f5d9ba754e6c5a72367067d6d4
NeeleshJoshi <neeleshjoshi@pop-os.localdomain>
2019-12-03 06:13:11 +0000

    Started on creating the VueX store

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+
+import Vuex, { Module } from "vuex";
+import SimpleStore from './store/index'
 
 Vue.config.productionTip = false
+
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  modules: {
+    SimpleStore
+  }
+})
+
 
 new Vue({
   router,
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,44 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators'
+import { TileInterface } from '@/components/Tile.stories';
+import { emptyData } from '@/components/Board.stories'
+import { defaultText } from '@/components/InfoText.stories'
 
-Vue.use(Vuex)
+@Module
+export default class SimpleStore extends VuexModule {
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
+  //State
+  boardState: Array<TileInterface> = emptyData;
+  infoText: string = defaultText;
+
+  //Getters
+  get getBoardState() {
+    return this.boardState;
   }
-})
+
+  get getInfoText() {
+    return this.infoText
+  }
+
+  //Mutations
+  @Mutation
+  setBoardState(state: Array<TileInterface>) {
+    this.boardState = state;
+  }
+
+  @Mutation
+  setInfoText(text: string) {
+    this.infoText = text;
+  }
+
+
+  //Actions
+  @Action({ commit: 'setBoardState' })
+  retBoardState(state: Array<TileInterface>) {
+    return state;
+  }
+
+  @Action({ commit: 'setInfoText' })
+  retInfoText(text: string) {
+    return text;
+  }
+}
