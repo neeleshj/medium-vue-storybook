5211da784211c5b4b612ca9a31a752b408824d6e
neelesH <neelesh@rolleragency.co.uk>
2019-12-17 14:09:24 +0000

    Installed storyshots and needed libraries

---
+++ tests/unit/storyshots.spec.js
@@ -0,0 +1,9 @@
+import initStoryshots from '@storybook/addon-storyshots';
+import registerRequireContextHook from 'babel-plugin-require-context-hook/register';
+
+registerRequireContextHook();
+initStoryshots({
+  /* configuration options */
+  configPath: 'config/storybook',
+  framework: 'vue',
+});
